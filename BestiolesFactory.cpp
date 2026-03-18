7300338d0ddf52b08a9d66028e98251841520e2d
Alan Eudo <alan.eudo@imt-atlantique.net>
2026-03-18 12:19:27 +0100

    Integration des capteurs et accessoires dans la factory

--- BestiolesFactory.cpp
+++ BestiolesFactory.cpp
@@ -4,6 +4,15 @@
 #include "Kamikaze.h"
 #include "MultiPersonnalite.h"
 #include "Milieu.h"
+#include "Yeux.h"
+#include "Oreilles.h"
+#include "Nageoires.h"
+#include "Carapace.h"
+#include "Camouflage.h"
+
+// La factory crée des bestioles complètement configurées
+// (comportement + capteurs + accessoires)
+// -> intégration des patterns Factory + Decorator
 
 BestiolesFactory* BestiolesFactory::instance = nullptr;
 
@@ -23,6 +32,10 @@ Bestiole* BestiolesFactory::createGregaire()
 {
     Bestiole* b = new Bestiole();
     b->setComportement( new Gregaire() );
+
+    b->ajouterCapteur( new Yeux( 1.5, 60.0, 1.0 ) );
+    b->ajouterAccessoire( new Nageoires( 1.3 ) );
+
     return b;
 }
 
@@ -30,6 +43,10 @@ Bestiole* BestiolesFactory::createPeureuse()
 {
     Bestiole* b = new Bestiole();
     b->setComportement( new Peureuse() );
+
+    b->ajouterCapteur( new Oreilles( 50.0, 1.0 ) );
+    b->ajouterAccessoire( new Camouflage( 0.5 ) );
+
     return b;
 }
 
@@ -37,16 +54,26 @@ Bestiole* BestiolesFactory::createKamikaze()
 {
     Bestiole* b = new Bestiole();
     b->setComportement( new Kamikaze() );
+
+    b->ajouterCapteur( new Yeux( 1.2, 70.0, 1.0 ) );
+    b->ajouterAccessoire( new Carapace( 0.3, 0.8 ) );
+
     return b;
 }
 
 Bestiole* BestiolesFactory::createMulti()
 {
     Bestiole* b = new Bestiole();
+
     MultiPersonnalite* mp = new MultiPersonnalite();
     mp->ajouterComportement( new Gregaire() );
     mp->ajouterComportement( new Kamikaze() );
+
     b->setComportement( mp );
+
+    b->ajouterCapteur( new Yeux( 1.5, 60.0, 1.0 ) );
+    b->ajouterCapteur( new Oreilles( 40.0, 1.0 ) );
+
     return b;
 }
 
